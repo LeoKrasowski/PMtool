e277fc18c7b99480d02f23d27e44e2363c64ca4a
LeoKrasowski <leokrasowski@gmail.com>
2023-11-06 19:42:21 +0100

    validation for fields

--- ppmtool-client/src/actions/projectActions.js
+++ ppmtool-client/src/actions/projectActions.js
@@ -23,6 +23,7 @@ export const createProject = (project, history) => async dispatch => {
 }
 
 export const getProjects = () => async dispatch => {
+    
     const res = await axios.get("http://localhost:8080/api/project/all");
     dispatch({
         type: GET_PROJECTS,
@@ -31,9 +32,13 @@ export const getProjects = () => async dispatch => {
 };
 
 export const getProject = (id, history) => async dispatch => {
+    try {
     const res = await axios.get(`http://localhost:8080/api/project/${id}`)
     dispatch({
         type:GET_PROJECT,
         payload: res.data
     });
+}catch (error) {
+    history.push("/dashboard")
+}
 };
