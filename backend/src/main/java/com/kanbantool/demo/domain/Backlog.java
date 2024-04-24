e277a8c9d1637d38b7eb80d20d50c7b246095cc5
LeoKrasowski <leokrasowski@gmail.com>
2024-04-24 21:01:06 +0200

    Added references

--- backend/src/main/java/com/kanbantool/demo/domain/Backlog.java
+++ backend/src/main/java/com/kanbantool/demo/domain/Backlog.java
@@ -1,9 +1,12 @@
 package com.kanbantool.demo.domain;
 
 import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.OneToOne;
 
 @Entity
 public class Backlog {
@@ -13,6 +16,16 @@ public class Backlog {
 	private Long id;
 	private Integer PTSequence = 0;
 	private String projectIndentifier;
+	
+	
+	//OneToOne with project
+	@OneToOne(fetch = FetchType.EAGER)
+	@JoinColumn(name="project_id",nullable = false)
+	private Project project;
+	
+	
+	//OneToMany projecttasks
+	
 	
 	public Backlog() {
 	}
@@ -39,6 +52,14 @@ public class Backlog {
 
 	public void setProjectIndentifier(String projectIndentifier) {
 		this.projectIndentifier = projectIndentifier;
+	}
+
+	public Project getProject() {
+		return project;
+	}
+
+	public void setProject(Project project) {
+		this.project = project;
 	}
 	
 	
--- backend/src/main/java/com/kanbantool/demo/domain/Project.java
+++ backend/src/main/java/com/kanbantool/demo/domain/Project.java
@@ -35,6 +35,10 @@ public class Project {
 	@JsonFormat(pattern = "yyyy-mm-dd")
 	private Date updated_At;
 	
+	@OneToOne(fetch = FetchType.EAGER, cascade = CascadeType.ALL, mappedBy = "project")
+	
+	private Backlog backlog;
+	
 	public Project() {
 		
 	}
@@ -104,6 +108,15 @@ public class Project {
 		this.updated_At = updated_At;
 	}
 
+	
+
+	public Backlog getBacklog() {
+		return backlog;
+	}
+
+	public void setBacklog(Backlog backlog) {
+		this.backlog = backlog;
+	}
 
 	@PrePersist
 	protected void onCreate() {
